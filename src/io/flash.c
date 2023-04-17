310bf5fd145ab320a7a7d9c860d38c6b6ce77178
Myles <mylesandmore9@gmail.com>
2023-04-16 23:05:43 -0500

    fix flash for realsies

--- src/io/flash.c
+++ src/io/flash.c
@@ -9,15 +9,15 @@ void flash_write(uint sector, float data[]) {
     // Disable interrupts because they can mess with our writing
     uint32_t intr = save_and_disable_interrupts();
     // Write our data (cast to an 8bit uint because that's the only type you can write to flash) and then restore interrupts
-    flash_range_program(offset, (uint8_t *) (data), FLASH_SECTOR_SIZE);
+    flash_range_program(offset, (uint8_t*)data, FLASH_SECTOR_SIZE);
     restore_interrupts (intr);
 }
 
 float flash_read(uint sector, uint val) {
     // Move the pointer forward to whatever address holds the requested value
-    float* calibration_val_ptr = (float*) ((FLASH_SECTOR_SIZE + sector * FLASH_SECTOR_SIZE) + (sizeof(float) * val));
-    // Retrieve and return the actual (dereferenced) calibration value
-    return *calibration_val_ptr;
+    float* val_ptr = (float*) ((XIP_BASE + (PICO_FLASH_SIZE_BYTES - (FLASH_SECTOR_SIZE + sector * FLASH_SECTOR_SIZE))) + (sizeof(float) * val));
+    // Retrieve and return the actual (dereferenced) value
+    return *val_ptr;
 }
 
 void flash_erase(uint sector) {
