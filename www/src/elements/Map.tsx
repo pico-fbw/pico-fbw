934133095f3681afc601df80685d90b7bc54f639
Thomas Burgeson <thomasebd123@gmail.com>
2025-02-09 16:13:04 -0600

    update button layout

--- www/src/elements/Map.tsx
+++ www/src/elements/Map.tsx
@@ -168,6 +168,10 @@ const Map: preact.FunctionComponent<MapProps> = ({ setIsFocused }) => {
         setEditing(id);
     };
 
+    const clearMarkers = () => {
+        setMarkers([]);
+    };
+
     /**
      * Event handler for when the map is clicked.
      * Adds a new marker at the clicked location.
@@ -692,7 +696,7 @@ const Map: preact.FunctionComponent<MapProps> = ({ setIsFocused }) => {
                                 <button
                                     type="button"
                                     onClick={() => removeMarker(editing ?? -1)}
-                                    className="sm:col-span-2 mt-auto rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                                    className="sm:col-span-2 mt-auto rounded-md bg-red-500/60 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                                 >
                                     Delete
                                 </button>
@@ -800,10 +804,34 @@ const Map: preact.FunctionComponent<MapProps> = ({ setIsFocused }) => {
             {/* If currently editing a waypoint, display the edit dock between the controls and table/buttons */}
             {editing >= 0 && <EditDock />}
             <div className="mx-auto max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
-                <div className="sm:flex sm:items-center">
+                <div className="sm:flex sm:items-center flex-row">
                     <div className="sm:flex-auto">
                         <h1 className="text-base font-semibold leading-6 text-white">Waypoints</h1>
                         <p className="mt-2 text-sm text-gray-300">A list of all waypoints in your flightplan</p>
+                    </div>
+                    <div className="relative flex-initial w-64 justify-end">
+                        <button
+                            type="button"
+                            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                            onClick={() => uploadFlightplan(JSON.stringify(markersToFlightplan(markers)))}
+                            className="mt-3 w-full rounded-md bg-white/10 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-white/20 cursor-pointer"
+                        >
+                            Upload
+                        </button>
+                        <button
+                            type="button"
+                            onClick={() => downloadFile(JSON.stringify(markersToFlightplan(markers)))}
+                            className="mt-3 w-full rounded-md bg-white/10 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-white/20 cursor-pointer"
+                        >
+                            Download
+                        </button>
+                        <button
+                            type="button"
+                            onClick={() => clearMarkers()}
+                            className="mt-3 w-full rounded-md bg-red-500/60 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500/90 cursor-pointer"
+                        >
+                            Clear
+                        </button>
                     </div>
                 </div>
                 <div className="mt-8 flow-root">
@@ -830,23 +858,6 @@ const Map: preact.FunctionComponent<MapProps> = ({ setIsFocused }) => {
                                         </Alert>
                                     )}
                                     <WaypointTable />
-                                    <div className="relative w-full">
-                                        <button
-                                            type="button"
-                                            // eslint-disable-next-line @typescript-eslint/no-misused-promises
-                                            onClick={() => uploadFlightplan(JSON.stringify(markersToFlightplan(markers)))}
-                                            className="mt-3 w-full rounded-md bg-white/10 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-white/20 cursor-pointer"
-                                        >
-                                            Upload
-                                        </button>
-                                        <button
-                                            type="button"
-                                            onClick={() => downloadFile(JSON.stringify(markersToFlightplan(markers)))}
-                                            className="mt-3 w-full rounded-md bg-white/10 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-white/20 cursor-pointer"
-                                        >
-                                            Download
-                                        </button>
-                                    </div>
                                 </div>
                             )}
                         </div>
